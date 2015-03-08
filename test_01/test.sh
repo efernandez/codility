49073c87674d102de7e77da73993621f7243301f
Enrique Fernandez <enrique.fernandez.perdomo@gmail.com>
2015-03-08 19:16:35 +0100

    Add test #1 (#(N, M) combination st N^2 + M^2 = Q)

---
+++ test_01/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Compile with:
+# mkdir build
+# cd build
+# cmake ..
+# make
+# cd ..
+
+# Default test:
+echo "2" | build/test_01
+
---
+++ test_01/test_01.h
@@ -0,0 +1,7 @@
+
+#ifndef TEST_01_H
+#define TEST_01_H
+
+int solution(int Q);
+
+#endif // TEST_01_H
